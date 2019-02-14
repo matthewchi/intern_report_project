906bf649788148dbf6b773992be967c1fe84d81f
Fisher Adelakin <temidayoadelakin@gmail.com>
2019-02-14 00:51:28 -0500

    working on css changes

--- internship_report.php
+++ internship_report.php
@@ -3,9 +3,10 @@
 <head>
     <title>Internship Report Form</title>
 
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+          integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 
-    <link href='https://fonts.googleapis.com/css?family=Archivo' rel='stylesheet'>
+    <link href='https://fonts.googleapis.com/css?family=Archivo:400,700' rel='stylesheet'>
 
     <link rel="stylesheet" type="text/css" href="css/style.css">
 
@@ -18,7 +19,7 @@
 
 <!-- top nav bar -->
 <div class="top-nav">
-    <?php include("includes/top-nav.php");?>
+    <?php include("includes/top-nav.php"); ?>
 </div>
 
 <!-- max width 760px -->
@@ -33,123 +34,145 @@
                 <div id="form1">
                     <h3>1/2</h3>
 
+                    <br>
 
-                    Company Information
+                    <h3>Company Information</h3>
 
-                    Name of Company<br>
-                    <input class="internship_report" type="text" name="company"><br>
+                    <br>
 
-                    Address of Main Office<br>
-                    <input class="internship_report" type="text" name="address"><br />
+                    <label> Name of Company <br>
+                        <input class="internship_report" type="text" name="company">
+                    </label><br>
 
-                    Type/Sector of CGT Industry - select all that apply<br>
-                    <input type="checkbox" name="cgtindustry1" value="animation" style="margin-top:4px;"> Animation<br>
-                    <input type="checkbox" name="cgtindustry2" value="construction"> Construction<br>
-                    <input type="checkbox" name="cgtindustry3" value="dataviz"> Data Visualization<br>
-                    <input type="checkbox" name="cgtindustry4" value="game"> Game<br>
-                    <input type="checkbox" name="cgtindustry5" value="ux"> User Experience Design<br>
-                    <input type="checkbox" name="cgtindustry6" value="vpi"> Virtual Product Integration<br>
-                    <input type="checkbox" name="cgtindustry7" value="visefx"> Visual Effects<br>
-                    <input class="internship_report" type="checkbox" name="cgtindustry8" value="web"> Web Programming and Design<br>
+                    <label>Address of Main Office<br>
+                        <input class="internship_report" type="text" name="address"><br/>
+                    </label>
+
+                    <br>
+                    Type/Sector of CGT Industry - select all that apply
+                    <br/>
+                    <label><input type="checkbox" name="cgtindustry1" value="animation" style="margin-top:4px;">
+                        Animation<br></label>
+                    <label><input type="checkbox" name="cgtindustry2" value="construction"> Construction<br></label>
+                    <label><input type="checkbox" name="cgtindustry3" value="dataviz"> Data Visualization<br></label>
+                    <label><input type="checkbox" name="cgtindustry4" value="game"> Game<br></label>
+                    <label><input type="checkbox" name="cgtindustry5" value="ux"> User Experience Design<br></label>
+                    <label><input type="checkbox" name="cgtindustry6" value="vpi"> Virtual Product
+                        Integration<br></label>
+                    <label><input type="checkbox" name="cgtindustry7" value="visefx"> Visual Effects<br></label>
+                    <label><input class="internship_report" type="checkbox" name="cgtindustry8" value="web"> Web
+                        Programming
+                        and Design<br></label>
 
 
                     Supervisor
 
-                    Name of Supervisor<br>
-                    <input class="internship_report" type="text" name="supervisorname"><br>
+                    <label>Name of Supervisor<br>
+                        <input class="internship_report" type="text" name="supervisorname"><br></label>
 
-                    Supervisor email<br>
-                    <input class="internship_report" type="email" name="supervisoremail"><br>
+                    <label>Supervisor email<br>
+                        <input class="internship_report" type="email" name="supervisoremail"><br></label>
 
-                    Supervisor phone<br>
-                    <input class="internship_report" type="text" name="supervisor phone"><br>
-
-
-
+                    <label>Supervisor phone<br>
+                        <input class="internship_report" type="text" name="supervisor phone"><br></label>
 
                     <button type="button" id="next" style="float:right;">Next</button>
 
                 </div>
 
                 <div id="form2" style="display:none;">
+                    <h3>2/2</h3>
 
-                    <fieldset>
-                        <legend>2/2</legend>
+                    <br>
 
-                        <fieldset>
-                            <legend>Job Information</legend>
+                    <h3>Job Information</h3>
 
-                            Did you work in an office or on site?<br>
-                            <input type="radio" name="paid" value="yes" style="margin-top:4px;"> Yes<br>
-                            <input class="internship_report" type="radio" name="paid" value="no"> No<br>
+                    <br>
 
-                            <fieldset>
-                                <legend>List 5 activites that you regularly performed during your internship.</legend>
-                                Activity 1<br>
-                                <input type="text" name="supervisor phone" style="margin-top:4px;"><br>
-                                Activity 2<br>
-                                <input type="text" name="supervisor phone" style="margin-top:4px;"><br>
-                                Activity 3<br>
-                                <input type="text" name="supervisor phone" style="margin-top:4px;"><br>
-                                Activity 4<br>
-                                <input type="text" name="supervisor phone" style="margin-top:4px;"><br>
-                                Activity 5<br>
-                                <input class="internship_report" type="text" name="supervisor phone"><br>
-                            </fieldset>
-                            Did your supervisor give you relevant work to accomplish - specify!<br>
-                            <input class="internship_report" type="text" name="supervisor phone"><br>
+                    <label>Did you work in an office or on site?
 
-                            Difficulties or problem areas encountered during internship<br>
-                            <input class="internship_report" type="text" name="supervisor phone"><br>
+                    <br>
 
-                            Explain how work experience related to your major<br>
-                            <input class="internship_report" type="text" name="supervisor phone"><br>
+                    <input type="radio" name="paid" value="yes" style="margin-top:4px;"> Yes<br>
+                    <input class="internship_report" type="radio" name="paid" value="no"> No<br></label>
 
-                            Is there anything you wanted to learn during your internship that you were not able to?<br>
-                            <input class="internship_report" type="text" name="supervisor phone"><br>
 
-                            Has this work experience changed your mind about which sector of CGT you might be most interested in pursuing?<br>
-                            <input class="internship_report" type="text" name="supervisor phone"><br>
+                    List 5 activities that you regularly performed during your internship.
+                    <br/>
+                    <br/>
+                    <label>Activity 1<br>
+                        <input type="text" name="supervisor phone" style="margin-top:4px;"><br></label>
+                    <br>
+                    Activity 2<br>
+                    <label><input type="text" name="supervisor phone" style="margin-top:4px;"><br></label>
+                    <br>
+                    <label>Activity 3<br>
+                        <input type="text" name="supervisor phone" style="margin-top:4px;"><br></label>
+                    <br>
+                    <label>Activity 4<br>
+                        <input type="text" name="supervisor phone" style="margin-top:4px;"><br></label>
+                    <br>
+                    <label>Activity 5<br>
+                        <input class="internship_report" type="text" name="supervisor phone"><br></label>
 
-                            Internship provided me with contacts which may lead to future employment<br>
-                            <input type="radio" name="paid" value="yes" style="margin-top:4px;"> Yes<br>
-                            <input class="internship_report" type="radio" name="paid" value="no"> No<br>
+                    <label>Did your supervisor give you relevant work to accomplish - specify!<br>
+                        <input class="internship_report" type="text" name="supervisor phone"><br></label>
 
-                            Considering your overall experience - how would you rate this internship?<br>
-                            <input type="radio" name="paid" value="yes" style="margin-top:4px;"> Very Dissatisfied<br>
-                            <input type="radio" name="paid" value="no"> Dissatisfied<br>
-                            <input type="radio" name="paid" value="yes"> Neutral<br>
-                            <input type="radio" name="paid" value="no"> Satisfied<br>
-                            <input class="internship_report" type="radio" name="paid" value="yes"> Very Satisfied<br>
+                    <label>Difficulties or problem areas encountered during internship<br>
+                        <input class="internship_report" type="text" name="supervisor phone"><br></label>
 
-                            <fieldset>
-                                <legend>Salary/Hourly Rate</legend>
+                    <label>Explain how work experience related to your major<br>
+                        <input class="internship_report" type="text" name="supervisor phone"><br></label>
 
-                                Were you paid?<br>
-                                <input type="radio" name="paid" value="yes" style="margin-top:4px;"> Yes<br>
-                                <input class="internship_report" type="radio" name="paid" value="no"> No<br>
+                    <label>Is there anything you wanted to learn during your internship that you were not able to?<br>
+                        <input class="internship_report" type="text" name="supervisor phone"><br></label>
 
-                                If so, how much?<br>
-                                <input class="internship_report" type="text" name="salary"><br>
+                    <label>Has this work experience changed your mind about which sector of CGT you might be most
+                        interested in
+                        pursuing?<br>
+                        <input class="internship_report" type="text" name="supervisor phone"><br></label>
 
-                                Did you receive a housing stipend?<br>
-                                <input type="radio" name="stipend" value="yes" style="margin-top:4px;"> Yes<br>
-                                <input class="internship_report" type="radio" name="stipend" value="no"> No<br>
+                    <label>Internship provided me with contacts which may lead to future employment<br>
+                        <input type="radio" name="paid" value="yes" style="margin-top:4px;"> Yes<br>
+                        <input class="internship_report" type="radio" name="paid" value="no"> No<br></label>
 
-                                Did you receive any other financial assistance from the company for your internship? Please select all that apply<br>
-                                <input type="checkbox" name="assist1" value="perdiem" style="margin-top:4px;"> Per Diem Allowance<br>
-                                <input type="checkbox" name="assist2" value="gas"> Gas Reimbursement<br>
-                                <input type="checkbox" name="assist3" value="vehicle"> Company Vehicle<br>
-                                <input type="checkbox" name="assist4" value="travel"> Paid Airfare or travel to/from internship location<br>
-                                <input class="internship_report" type="checkbox" name="assist5" value="other"> Other<br>
+                    Considering your overall experience - how would you rate this internship?<br>
+                    <label><input type="radio" name="paid" value="yes" style="margin-top:4px;"> Very
+                        Dissatisfied<br></label>
+                    <label><input type="radio" name="paid" value="no"> Dissatisfied<br></label>
+                    <label><input type="radio" name="paid" value="yes"> Neutral<br></label>
+                    <label><input type="radio" name="paid" value="no"> Satisfied<br></label>
+                    <label><input class="internship_report" type="radio" name="paid" value="yes"> Very
+                        Satisfied<br></label>
+                    <h3>Salary/Hourly Rate</h3>
 
-                            </fieldset>
-                        </fieldset>
+                    <br>
 
-                        <input type="submit" value="Submit">
-                        <button type="button" id="back">Back</button>
+                    Were you paid?<br>
+                    <label><input type="radio" name="paid" value="yes" style="margin-top:4px;"> Yes<br></label>
+                    <label><input class="internship_report" type="radio" name="paid" value="no"> No<br></label>
 
-                    </fieldset>
+                    If so, how much?<br>
+                    <label><input class="internship_report" type="text" name="salary"><br></label>
+
+                    Did you receive a housing stipend?<br>
+                    <label><input type="radio" name="stipend" value="yes" style="margin-top:4px;"> Yes<br></label>
+                    <label><input class="internship_report" type="radio" name="stipend" value="no"> No<br></label>
+
+                    Did you receive any other financial assistance from the company for your internship? Please select
+                    all that apply<br>
+                    <label><input type="checkbox" name="assist1" value="perdiem" style="margin-top:4px;"> Per Diem
+                        Allowance<br></label>
+                    <label><input type="checkbox" name="assist2" value="gas"> Gas Reimbursement<br></label>
+                    <label><input type="checkbox" name="assist3" value="vehicle"> Company Vehicle<br></label>
+                    <label><input type="checkbox" name="assist4" value="travel"> Paid Airfare or travel to/from
+                        internship
+                        location<br></label>
+                    <label><input class="internship_report" type="checkbox" name="assist5" value="other">
+                        Other<br></label>
+
+                    <input type="submit" value="Submit" id="internship-report-submit">
+                    <button type="button" id="back">Back</button>
 
                 </div>
             </form>
