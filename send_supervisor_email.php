4d3cb68cb9b1bb07a067c5d944eb85b0bb072e15
Varun Ramakrishnan <oceans.varun@gmail.com>
2019-02-28 11:08:45 -0500

    Got email to work

--- send_supervisor_email.php
+++ send_supervisor_email.php
@@ -6,15 +6,15 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 /* Exception class. */
-require 'PHPMailer-master\src\Exception.php';
+require 'PHPMailer-master/src/Exception.php';
 
 /* The main PHPMailer class. */
-require 'PHPMailer-master\src\PHPMailer.php';
+require 'PHPMailer-master/src/PHPMailer.php';
 
 /* SMTP class, needed if you want to use SMTP. */
-require 'PHPMailer-master\src\SMTP.php';
+require 'PHPMailer-master/src/SMTP.php';
 
-$email = new PHPMailer();
+$mail = new PHPMailer();
 
    /**
     * Set all the attributes for $mail using the
@@ -22,83 +22,25 @@ $email = new PHPMailer();
     * Change the attributes accordingly
     */
 
-   /* Set the mail sender. */
-   $mail->setFrom('cgt456group3@gmail.com', 'CGT 456 - Group 3');
-
-   /* Add a recipient. */
-   // Change value to supervisor's email from session variables
-   $mail->addAddress('oceans.varun@gmail.com', 'Varun');
-
-   /* Set the subject. */
-   $mail->Subject = 'Test';
-
-   /* Set the mail message body. */
-   $mail->Body = 'This is a test message';
-
-   // Make it use SMTP
-   $mail->isSMTP();
-
-   // Set GMail as the host for the emails
-   $mail->Host = 'smtp.gmail.com';
-
-   // Use 587 as the port
-   $mail->Port = 587;
-
-   // Authenticate the mal
-   $mail->SMTPAuth = true;
-
-   // Make it secure using tls
-   $mail->SMTPSecure = 'tls';
-   
-   /* Username (email address). */
-   $mail->Username = 'cgt456group3@gmail.com';
-   
-   /* Google account password. */
-   $mail->Password = 'goltZbach!';
-
-   /* Finally send the mail. */
-   $mail->send();
-
-      /* Finally send the mail. */
-   if (!$mail->send())
-   {
-      /* PHPMailer error. */
-      echo $mail->ErrorInfo;
-   }
-
+    $mail->IsSMTP(); // enable SMTP
+    $mail->SMTPDebug = 1; // debugging: 1 = errors and messages, 2 = messages only
+    $mail->SMTPAuth = true; // authentication enabled
+    $mail->SMTPSecure = "ssl"; // secure transfer enabled REQUIRED for Gmail
+    $mail->Host = "smtp.gmail.com";
+    $mail->Port = 465; // or 587
+    $mail->IsHTML(true);
+    $mail->Username = "cgt456group03@gmail.com";
+    $mail->Password = "glotZbach!";
+    $mail->SetFrom("cgt456group03@gmail.com");
+    $mail->Subject = "Test";
+    $mail->Body = "This is a test message";
+    $mail->AddAddress("oceans.varun@gmail.com");
+    
+     if(!$mail->Send()) {
+        echo "Mailer Error: " . $mail->ErrorInfo;
+     } 
+     else {
+        echo "Message has been sent";
+     }
 
 ?>
-
-<!--
-<html>
-   
-   <head>
-      <title>Sending HTML email using PHP</title>
-   </head>
-   
-   <body>
-      
-      <?php
-         /*$to = $_POST["intern1_5_supervisor_email"];
-         $subject = "Complete Intership Report";
-         
-         $message = "<h1>Complete Internship Report for student</h1>";
-         $message .= "<b>A student has mentioned you as their supervisor</b>";
-         $message = "<b>Please click on the following link to complete the report </b>";
-         $message = "<b><a href = \"/supervisor_report.php> Supervisor Report </a> </b>";
-
-         $header = "From:Purdue Internship Report \r\n";
-         $header .= "Cc:" + $_POST[ "ca_email" ] + "\r\n";
-         
-         $retval = mail ($to,$subject,$message,$header);
-         
-         if( $retval == true ) {
-            echo "Message sent successfully...";
-         }
-         else {
-            echo "Message could not be sent...";
-         }*/
-      ?>
-      
-   </body>
-</html>-->
