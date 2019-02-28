b9e00dac4cbed525c9929ee47e16d98432befc7e
Tyler Simon <simont@purdue.edu>
2019-02-28 01:18:03 -0500

    Top-nav changes

--- includes/top-nav.php
+++ includes/top-nav.php
@@ -2,14 +2,21 @@
 
     <div class="container">
         <li class="top-nav brand">
-            <i class="fab fa-pied-piper-pp"></i>
+            <a href="home.php"><i class="fab fa-pied-piper-pp"></i></a>
         </li>
 
         <li class="top-nav search">
             <form class="top-nav"><input class="top-nav" placeholder="Search"/><i class="fas fa-search"></i></form>
         </li>
 
-        <li class="top-nav options" style="float:right"><i class="fas fa-caret-down"></i></li>
+        <li class="top-nav options" style="float:right">
+            <div class="dropdown">
+                <button class="dropButton"><i class="fas fa-caret-down"></i></button>
+                <div class="dropContent">
+                    <a href="index.php">Log Out</a>
+                </div>
+            </div>
+        </li>
 
         <li class="top-nav" style="float:right"><a class="top-nav" href="internship_home.php">Internships</a></li>
         <li class="top-nav" style="float:right"><a class="top-nav" href="#capstone">Capstones</a></li>
